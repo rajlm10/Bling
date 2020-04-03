099ea434f60f6f3f400e8725b48827441e49d1f0
Raj Sangani <rajsangani@Rajs-MacBook-Air.local>
2020-04-03 19:52:57 +0530

    =setup prod webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,48 @@
-const path=require('path')
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
+module.exports = (env) => {
+  const isProduction = env === 'production';
+  const CSSExtract = new ExtractTextPlugin('styles.css');
 
-module.exports=(env)=>{
-    const isProduction=env==='production'
-
-    
-    return {
-        entry:'./src/app.js',
-        output:{
-            path:path.join(__dirname,'public'),    //needs to be absolute
-            filename:'bundle.js'
-        },
-        module:{
-            rules:[{
-                loader:'babel-loader',
-                test:/\.js$/,
-                exclude:/node_modules/
-            },{
-                test:/\.s?css$/,
-                use:['style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
-            }]
-        },
-        devtool:isProduction?'source-map':'cheap-module-eval-source-map',
-        devServer:{
-            contentBase:path.join(__dirname,'public'),
-            historyApiFallback: true
-        }
+  return {
+    entry: './src/app.js',
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [{
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      }, {
+        test: /\.s?css$/,
+        use: CSSExtract.extract({
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true
+              }
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true
+              }
+            }
+          ]
+        })
+      }]
+    },
+    plugins: [
+      CSSExtract
+    ],
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: true
     }
-}
-
-
-
-//loader
+  };
+};
